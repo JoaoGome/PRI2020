72221edfdaa8e5551345b50be5accde553dca6bc
Johnny <johnny@Johnnys-MBP.lan>
2020-11-29 11:32:49 +0000

    TPC7

--- TP7/PRI2020/app.js
+++ TP7/PRI2020/app.js
@@ -15,7 +15,7 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error...'));
 db.once('open', function() {
-    console.log("Conexão ao MongoDB realizada com sucesso...")
+    console.log("Conexão ao MongoDB realizada com sucesso......")
 });
 
 var indexRouter = require('./routes/index');
